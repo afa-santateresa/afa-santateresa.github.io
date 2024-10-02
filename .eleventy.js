8845bc5fe106c5479425dd3a81fc8d2eb6fe3259
Jorge Sanz <jorge@jorgesanz.net>
2024-10-02 10:41:29 +0200

    Migrate to eleventy@v3

--- .eleventy.js
+++ .eleventy.js
@@ -1,68 +1,63 @@
 // docs: https://www.11ty.io/docs/config/
-const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
 
-const markdownIt = require("markdown-it");
-const markdownItAnchor = require("markdown-it-anchor");
-const pluginTOC = require('eleventy-plugin-toc');
+import markdownIt from "markdown-it";
+import markdownItAnchor from "markdown-it-anchor";
+import pluginTOC from "eleventy-plugin-toc";
 
-const { DateTime } = require("luxon");
-const yaml = require("js-yaml");
+import { DateTime } from "luxon";
+import yaml from "js-yaml";
 
-module.exports = function(eleventyConfig) {
+
+export default async function(eleventyConfig) {
   // Options for the `markdown-it` library
-  const markdownItOptions = { html: false, }
+  const markdownItOptions = { html: false };
 
   // Options for the `markdown-it-anchor` library
-  const markdownItAnchorOptions = { permalink: false, }
+  const markdownItAnchorOptions = { permalink: false };
 
   const markdownLib = markdownIt(markdownItOptions).use(
     markdownItAnchor,
     markdownItAnchorOptions
-  )
+  );
 
-  eleventyConfig.setLibrary("md", markdownLib)
+  eleventyConfig.setLibrary("md", markdownLib);
 
   // eleventyConfig.addFilter( "myFilter", function() {});
-  
+
   eleventyConfig.setBrowserSyncConfig({
     // https://www.browsersync.io/docs/options/#option-ghostMode
-    ghostMode: false
+    ghostMode: false,
   });
 
-
-	eleventyConfig.addPassthroughCopy({
+  eleventyConfig.addPassthroughCopy({
     "static/root/*": "/",
     "static/css/*": "assets/css/",
     "static/imgs/*": "assets/imgs/",
     "static/favicon/*": "assets/favicon/",
     "static/docs/*": "assets/docs/",
-    "static/admin/*": "admin/"
+    "static/admin/*": "admin/",
   });
 
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
   eleventyConfig.addPlugin(pluginTOC);
 
   eleventyConfig.addFilter("comAData", (dateObj) => {
-    return DateTime
-      .fromJSDate(dateObj)
-      .setLocale('ca')
+    return DateTime.fromJSDate(dateObj)
+      .setLocale("ca")
       .toLocaleString(DateTime.DATE_MED);
-   });
-
+  });
 
   eleventyConfig.addFilter("comAny", (dateObj) => {
-    return DateTime
-      .fromJSDate(dateObj)
-      .toFormat('yyyy')
-   });
+    return DateTime.fromJSDate(dateObj).toFormat("yyyy");
+  });
 
-
-  eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
+  eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
 
   return {
     dir: {
       input: "src",
-      output: "dist"
-    }
+      output: "dist",
+    },
   };
 };
--- src/_data/documents.js
+++ src/_data/documents.js
@@ -1,15 +1,13 @@
-require("dotenv").config();
-const EleventyFetch = require("@11ty/eleventy-fetch");
+import "dotenv/config";
+import EleventyFetch from "@11ty/eleventy-fetch";
 
-const {
-  GOOGLE_SHEET_ID,
-  GOOGLE_API_KEY,
-  GOOGLE_SHEET_NAME
-} = process.env;
+const { GOOGLE_SHEET_ID, GOOGLE_API_KEY, GOOGLE_SHEET_NAME } = process.env;
 
 const URL = `https://sheets.googleapis.com/v4/spreadsheets/${GOOGLE_SHEET_ID}/values/${GOOGLE_SHEET_NAME}?key=${GOOGLE_API_KEY}`;
 
-module.exports = async function () {
+console.log(URL)
+
+export default async function () {
   // https://developer.github.com/v3/repos/#get
   let json = await EleventyFetch(URL, {
     duration: "1m", // 1 day
@@ -18,25 +16,27 @@ module.exports = async function () {
 
   // la primera fila es la capçalera
   const docs = json.values.slice(1);
-  console.log('Documents', docs.length);
+  console.log("Documents", docs.length);
 
   // extrau els cursos
-  const cursos = [... new Set(docs.map(e => e[1]))];
+  const cursos = [...new Set(docs.map((e) => e[1]))];
 
   // genera una entrada per curs amb els documents
-  const documents = cursos.map( curs => {
+  const documents = cursos.map((curs) => {
     return {
-        curs,
-        documents: docs.filter( d => d[1] == curs).map(d => {
-            return {
-                'tipus': d[0],
-                'data': d[2],
-                'titol': d[3],
-                'url': d[4]
-            }
-        })
-    }
-  })
+      curs,
+      documents: docs
+        .filter((d) => d[1] == curs)
+        .map((d) => {
+          return {
+            tipus: d[0],
+            data: d[2],
+            titol: d[3],
+            url: d[4],
+          };
+        }),
+    };
+  });
 
   return documents;
-};
+}
